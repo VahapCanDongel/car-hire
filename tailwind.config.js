97c1b8da1fa6e07a23ed3c7821ebdcef42d20ce2
v-Alqorith <vahapcandongel@hotmail.com>
2023-05-10 19:17:53 +0100

    Chnaged font

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,21 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
     screens: {
-			xl: { max: "1279px" },
-			// => @media (max-width: 1279px) { ... }
+      xl: { max: "1279px" },
+      // => @media (max-width: 1279px) { ... }
 
-			lg: { max: "1023px" },
-			// => @media (max-width: 1023px) { ... }
+      lg: { max: "1023px" },
+      // => @media (max-width: 1023px) { ... }
 
-			sm: { max: "767px" },
-			// => @media (max-width: 639px) { ... }
-		},
+      sm: { max: "767px" },
+      // => @media (max-width: 639px) { ... }
+    },
+    fontFamily: {
+      poppins: ["Poppins", "sans-serif"],
+    },
   },
   plugins: [],
-}
+};
