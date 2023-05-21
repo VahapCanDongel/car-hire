cb082d7800f5fcbf0bbb82504eaaa5d7e22b4f54
v-Alqorith <vahapcandongel@hotmail.com>
2023-05-21 13:56:26 +0100

    Added actual testimonials

--- src/components/Testimonial.jsx
+++ src/components/Testimonial.jsx
@@ -1,17 +1,16 @@
-export default function Testimonial() {
+export default function Testimonial({ user_review, user_name, user_location }) {
   return (
     <div>
-      <div className="border w-[600px] p-10 rounded-sm flex justify-center items-center flex-col sm:w-[400px]">
+      <div className="border w-[600px] p-10 rounded-sm flex justify-center items-center flex-col sm:w-[400px] min-h-[430px] my-8">
         <p className="w-[450px] text-[20px] font-semibold sm:text-[15px] sm:w-[350px]">
-          "Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit
-          similique hic nostrum distinctio vel! Quidem, ad officiis. Eligendi
-          provident nam.
+
+          {user_review}
         </p>
 
         <div className="flex justify-between w-3/4 relative sm:w-full">
           <div>
-            <div className="font-bold text-[22px] mt-4 sm:text-[18px]">John Doe</div>
-            <div className="sm:text-[14px]">Belgrade</div>
+            <div className="font-bold text-[22px] mt-4 sm:text-[18px]">{user_name}</div>
+            <div className="sm:text-[14px]">{user_location}</div>
           </div>
 
           <div className="font-semibold font-space text-[205px] text-orange-400 h-[35px] absolute bottom-[100px] left-[380px] sm:left-[260px]">
--- src/sections/Testimonials.jsx
+++ src/sections/Testimonials.jsx
@@ -15,8 +15,8 @@ export default function Testimonials() {
 
 
             <div className="flex gap-8 justify-center sm:flex-col">
-                <Testimonial />
-                <Testimonial />
+                <Testimonial user_name={"John Doe"} user_location={"London"} user_review={"Car Hire exceeded my expectations in every way possible. From the moment I contacted their customer service team, I knew I was in good hands. They were prompt, courteous, and extremely knowledgeable, answering all my questions and guiding me through the rental process with ease. Their dedication to customer satisfaction was evident throughout the entire experience."} />
+                <Testimonial user_name={"Steven Java"} user_location={"Manchester"} user_review={"Car Hire deserves all the praise for their exceptional customer service and top-quality vehicles. When I contacted their customer support team, I was immediately greeted with friendly and professional assistance. They patiently walked me through the reservation process, helping me find the perfect vehicle for my needs."} />
             </div>
 
 
